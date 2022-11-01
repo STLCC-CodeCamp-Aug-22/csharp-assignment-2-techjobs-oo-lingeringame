b42c4468e4d804b5023daf7c1684d02f5d97e004
sidereal <holasoyserg@gmail.com>
2022-11-01 00:23:35 -0500

    added new abstract base class

--- TechJobsOOAutograded/CoreCompetency.cs
+++ TechJobsOOAutograded/CoreCompetency.cs
@@ -3,40 +3,13 @@ using TechJobsOOAutograded;
 
 namespace TechJobsOO
 {
-    public class CoreCompetency
+    public class CoreCompetency : JobField
     {
 
-        public int Id { get; set; }
-        private static int nextId = 1;
-        public string Value { get; set; }
-
         // TODO: Change the fields to auto-implemented properties.
-
-        public CoreCompetency()
-        {
-            Id = nextId;
-            nextId++;
-        }
-
-        public CoreCompetency(string v) : this()
+        public CoreCompetency(string v) : base(v)
         {
             Value = v;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is CoreCompetency competency &&
-                   Id == competency.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
         }
 
     }
